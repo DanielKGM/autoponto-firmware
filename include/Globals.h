35c7bb798bb49225c041e53c2b4596c587099e3f
DanielKGM <atbreezy4@gmail.com>
2025-12-30 11:53:35 -0300

    feat: contexto das aulas

--- include/Globals.h
+++ include/Globals.h
@@ -3,9 +3,7 @@
 #include <Preferences.h>
 #define EVT_SLEEP (1 << 0)
 
-// BOOTING -> NET_OFF -> NET_ON -> MQTT_OFF -> WORKING -> WAITING_SERVER || IDLE || NET_OFF || MQTT_OFF
-// WAITING_SERVER -> WORKING
-// IDLE -> SLEEPING
+#define FB_MSG_LENGTH 256
 
 enum class SystemState
 {
@@ -19,12 +17,34 @@ enum class SystemState
     SLEEPING
 };
 
+enum ContextFields
+{
+    MSG = 1 << 0,
+    MS_NEXT = 1 << 1,
+    MS_REM = 1 << 2,
+    SYNCED = 1 << 3,
+    UPDATE = 1 << 4,
+    ALL = 0xFF
+};
+
+struct AttendanceContext
+{
+    char msg[FB_MSG_LENGTH];
+    unsigned long msForNext;
+    unsigned long msRemaining;
+    bool synced;
+    bool shouldUpdate;
+};
+
+extern AttendanceContext context;
 extern EventGroupHandle_t systemEvents;
 extern char deviceId[13];
 extern TaskHandle_t TaskDisplay;
 extern TaskHandle_t TaskNetwork;
 extern TaskHandle_t TaskMqtt;
 
+void saveContext(uint8_t fields = ALL);
+void loadContext(uint8_t fiels = ALL);
 void changeTaskCount(short int delta);
 short int checkTaskCount();
 void setState(SystemState newState);
--- src/Globals.cpp
+++ src/Globals.cpp
@@ -12,6 +12,54 @@ EventGroupHandle_t systemEvents = xEventGroupCreate();
 TaskHandle_t TaskDisplay;
 TaskHandle_t TaskNetwork;
 TaskHandle_t TaskMqtt;
+AttendanceContext context = AttendanceContext{};
+
+void saveContext(uint8_t flags)
+{
+    Preferences prefs;
+    prefs.begin("context", false);
+
+    if (flags & MSG)
+        prefs.putString("msg", context.msg);
+
+    if (flags & MS_NEXT)
+        prefs.putULong("msForNext", context.msForNext);
+
+    if (flags & MS_REM)
+        prefs.putULong("msRemaining", context.msRemaining);
+
+    if (flags & SYNCED)
+        prefs.putBool("synced", context.synced);
+
+    if (flags & UPDATE)
+        prefs.putBool("shouldUpdate", context.shouldUpdate);
+
+    prefs.end();
+}
+
+void loadContext(uint8_t flags)
+{
+    Preferences prefs;
+
+    prefs.begin("context", true);
+
+    if ((flags & MSG) && prefs.isKey("msg"))
+        prefs.getString("msg", context.msg, sizeof(context.msg));
+
+    if (flags & MS_NEXT)
+        context.msForNext = prefs.getULong("msForNext", context.msForNext);
+
+    if (flags & MS_REM)
+        context.msRemaining = prefs.getULong("msRemaining", context.msRemaining);
+
+    if (flags & SYNCED)
+        context.synced = prefs.getBool("synced", context.synced);
+
+    if (flags & UPDATE)
+        context.shouldUpdate = prefs.getBool("shouldUpdate", context.shouldUpdate);
+
+    prefs.end();
+}
 
 void changeTaskCount(short int delta)
 {
